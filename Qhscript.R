ea9d7febdb3c53c1f9006e236785b2fe1e3cf5a7
Péter Kalicz <kaliczp@gmail.com>
2018-11-29 13:20:50 +0100

    Last used script is commited

---
+++ Qhscript.R
@@ -0,0 +1,11 @@
+scan(file = "Q.txt", dec = ",")
+scan(dec = ",", file = "Q.txt")
+Q <- scan("Q.txt", dec = ",")
+qm <- matrix(Q, ncol = 10)
+qm[,1]
+h <- scan("h.txt", dec = ",")
+Qh <- cbind(Q,h)
+plot(Qh[,1],Qh[,2])
+colnames(Qh)
+plot(Qh[,"Q"],Qh[,"h"])
+## Try to plot Q<10 [l/s]
